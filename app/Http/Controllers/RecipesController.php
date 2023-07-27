58c6dc390969c318be850302885008a39b29c3c4
Vergazov_A <vergazka@gmail.com>
2023-07-27 15:55:21 +0300

    добавил методы в контроллер

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -9,4 +9,28 @@ class RecipesController extends Controller
     public function index(){
         return view('recipes.index');
     }
+
+    public function create(){
+        dd('create');
+    }
+
+    public function store(){
+        dd('store');
+    }
+
+    public function show(){
+        dd('show');
+    }
+
+    public function edit(){
+        dd('edit');
+    }
+
+    public function update(){
+        dd('update');
+    }
+
+    public function destroy(){
+        dd('destroy');
+    }
 }
